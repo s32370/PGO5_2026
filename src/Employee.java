838f4ca6015ee055f783f5a97f63519772ff65dc
Herman <s32370@pjwstk.edu.pl>
2026-04-14 12:52:12 +0200

    Employee class added

---
+++ src/Employee.java
@@ -0,0 +1,19 @@
+public class Employee {
+
+   // Example fields:
+
+    private String employeeId
+    private String firstName
+    private String lastName
+
+    private double baseSalary
+    //This class may contain:
+
+    //a constructor that initializes the shared fields,
+    //getters for shared data,
+    a method public final String getEmployeeId(),
+    a method public double calculateMonthlyCost(),
+    a method public String introduceYourself(),
+    an overridden toString(),
+    an overridden equals() comparing objects by employeeId.
+}
