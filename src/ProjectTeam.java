50ee844a9d718ff3bfa7802ded5eac536889169d
Herman <s32370@pjwstk.edu.pl>
2026-04-14 13:34:14 +0200

    project team

--- src/ProjectTeam.java
+++ src/ProjectTeam.java
@@ -1,15 +1,53 @@
+import java.util.ArrayList;
+
 public class ProjectTeam {
-    //A class managing the list of employees.
 
-   // Example fields:
-   //
-   private String projectName;
-   private ArrayList<Employee>; employees
-   // Example methods:
+    private String projectName;
+    private ArrayList<Employee> employees = new ArrayList<>();
 
-    addEmployee(Employee employee)
-    printTeamMembers()
-    calculateTotalCost()
-    findById(String employeeId)
-    printDevelopers() or printAutomationTesters() using instanceof.
+    // Example methods:
+    public ProjectTeam(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    public void printTeamMembers() {
+        System.out.println();
+    }
+
+    public double calculateTotalCost() {
+        double Total = 0;
+        for (Employee e : employees) {
+            Total += e.calculateMonthlyCost();
+        }
+        return Total;
+    }
+
+    public Employee findById(String employeeId) {
+        for (Employee e : employees) {
+            if (e.getEmployeeId().equals(employeeId)) {
+                return e;
+            }
+        }
+    }
+
+    public void printDevelopers() { //or
+        for (Employee e : employees){
+            if (e instanceof Developer){
+                System.out.println(e);
+            }
+        }
+    }
+
+    public void printAutomationTesters() { //using instanceof.
+        for (Employee e : employees){
+            if (e instanceof Tester){
+                System.out.println(e);
+            }
+        }
+    }
+
 }
